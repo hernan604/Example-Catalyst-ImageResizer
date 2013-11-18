eda5de91c203683e04efee26adfb09baade8de87
Hernan Lopes <aut_hlopes@uolinc.com>
2013-11-18 20:47:29 -0200

    allows the user to set dev-server port

--- start_app.sh
+++ start_app.sh
@@ -1,1 +1,6 @@
-perl -I./ResizerRole/lib/ -I./Catalyst-App/lib/ ./Catalyst-App/script/resizer_server.pl -rdp 9999
+#usage: sh start_app.sh 9999
+
+PORT=$1
+: ${PORT:="9999"}
+perl -I./ResizerRole/lib/ -I./Catalyst-App/lib/ ./Catalyst-App/script/resizer_server.pl -rdp $PORT
+
